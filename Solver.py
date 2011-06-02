802116b76bd50b9e69c335a6ad422597c43e6ab0
Bjørnar Snoksrud <snoksrud@gmail.com>
2011-06-02 16:02:32 +0200

    Added hidden singles

--- Solver.py
+++ Solver.py
@@ -13,7 +13,7 @@ class Solver:
         while self.b.todo:
             deadlock = True
             for method in methods:
-                if method(self.b):
+                if method(self):
                     break
             else:
                 print 'stuck!'
@@ -21,20 +21,37 @@ class Solver:
         if not self.b.todo:
             print 'EXCELSIOR!'
 
+    def do(self, n, v, solve):
+        print '(%d,%d) = %d - %s' % (n.x + 1, n.y +1, v +1, solve)
+        self.b.setNum(n, v)
+
     # solve methods
     @solve_mtd
-    def singles(b):
-        for n in b.todo:
+    def singles(self):
+        for n in self.b.todo:
+            # if a number only has one options, take it
             if len(n.options) == 1:
-                print '(%d,%d) = %d - single' % (n.x + 1, n.y +1, n.options[0] +1)
-                b.setNum(n, n.options[0])
+                self.do(n, n.options[0], 'single')
                 return True
 
         return False
  
-#    @solve_mtd
-    def hidden_singles(b):
+    @solve_mtd
+    def hidden_singles(self):
+        for grp in self.b.groups.values():
+            for line in grp.values():
+                # for each 'line' in a group, check if a candidate is unique, if so take it
+
+                for guess in range(self.b.dim):
+                    n = 0
+                    cand = None
+                    for num in line:
+                        if guess in num.options:
+                            n += 1
+                            cand = num
+                    if n == 1:
+                        self.do(cand, guess, 'hidden single')
+                        return True
+
         return False
-        pass
- 
  
